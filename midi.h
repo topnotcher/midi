62b95a7b23c20ec6cbddd78112560812b0456432
Greg Bowser <topnotcher@gmail.com>
2013-04-26 21:53:28 -0400

    data types for parsing a track

--- midi.h
+++ midi.h
@@ -17,13 +17,44 @@ typedef struct {
 	uint32_t size;
 } midi_track_hdr_t;
 
-typedef struct {
-	midi_track_hdr_t hdr; 		
-} midi_track_t;
+#define MIDI_TYPE_EVENT
+#define MIDI_TYPE_META
 
 typedef struct {
-	uint32_t t; //time delta...
+	uint8_t cmd;
+	uint8_t size;
+	uint8_t data[];
+} midi_meta_t;
+
+typedef struct {
 	uint8_t cmd;
 	uint8_t chan;
-	uint8_t * data; 
+	uint8_t data[]; 
 } midi_event_t;
+
+
+typedef struct midi_event_node_s {
+	uint32_t td;
+	union {
+		midi_event_t event;
+		midi_meta_t meta;
+	};
+	struct midi_event_node_s * next;
+} midi_event_node_t;
+
+
+typedef struct {
+	midi_track_hdr_t hdr;
+	uint32_t events;
+	midi_event_node_t * head;
+	midi_event_node_t * current;
+} midi_track_t;
+
+
+#define MIDI_EVENT_NOTE_OFF 		0x08
+#define MIDI_EVENT_NOTE_ON 		0x09
+#define MIDI_EVENT_KEY_AFTER_TOUCH	0x0A
+#define MIDI_EVENT_CONTROL_CHANGE	0x0B
+#define MIDI_EVENT_PATH_CHANGE		0x0C
+#define MIDI_EVENT_CHANNEL_AFTER_TOUCH	0x0D
+#define MIDI_EVENT_PITCH_WHEEL_CHANGE	0x0E
