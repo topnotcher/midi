129b798180e9975a5f70f4fa359cd95fbaeca388
Greg Bowser <topnotcher@gmail.com>
2013-04-30 21:45:58 -0400

    Add an iterator interface

--- midi.c
+++ midi.c
@@ -244,3 +244,17 @@ static inline char * midi_get_eventstr(uint8_t cmd) {
 	return "???";
 }
 
+
+void midi_iter_track(midi_track_t * trk) {
+	trk->cur = trk->head;
+}
+bool midi_track_has_next(midi_track_t *trk) {
+	return trk->cur != NULL;
+}
+
+//yeah brillaint - just expose the node!
+midi_event_node_t * midi_track_next(midi_track_t *trk) {
+	midi_event_node_t * cur = trk->cur;
+	trk->cur = trk->cur->next;
+	return cur;
+}
